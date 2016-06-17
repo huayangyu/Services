c08d87fb4b4e255e3fc391b7f676168cc5c64486
root <lovehuayangyu@163.com>
2016-06-17 10:36:43 +0800

    Initial Commit

---
+++ XYService/XYService.h
@@ -0,0 +1,13 @@
+//
+//  XYService.h
+//  XYService
+//
+//  Created by Charlie on 16/6/17.
+//  Copyright © 2016年 XY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYService : NSObject
+
+@end
---
+++ XYService/XYService.m
@@ -0,0 +1,13 @@
+//
+//  XYService.m
+//  XYService
+//
+//  Created by Charlie on 16/6/17.
+//  Copyright © 2016年 XY. All rights reserved.
+//
+
+#import "XYService.h"
+
+@implementation XYService
+
+@end
